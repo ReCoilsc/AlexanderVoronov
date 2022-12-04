a7aa157b3edb25145cca16fee0a385c7b261a56d
ReCoilsc <117853700+ReCoilsc@users.noreply.github.com>
2022-12-04 23:07:15 +0300

    Update BubblesortVORONOV.cpp

--- BubblesortVORONOV.cpp
+++ BubblesortVORONOV.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 template <typename T>
-void bubbleSort(T arr[], int arrLength)
+void bubbleSort(T arr[], int arrLength) //реализация сортировки
 {
 	for (int i = 0; i < arrLength; i++) {
 		for (int j = 0; j < arrLength - 1; j++) {
-			T temp;
+			T temp;// без swap, временная переменная для замены элементов массива
 			if (arr[j] > arr[j + 1]) {
 				temp = arr[j + 1];
 				arr[j + 1] = arr[j];
@@ -17,7 +17,7 @@ void bubbleSort(T arr[], int arrLength)
 }
 int main()
 {
-	int arr[] = { 1,9,7,4,11 };
+	int arr[] = { 1,9,7,4,11 };// для int
 	int size_arr = sizeof(arr) / sizeof(arr[0]);
 	std::cout << "size_arr = " << size_arr << std::endl;
 
@@ -27,7 +27,7 @@ int main()
 		std::cout << arr[i] << " ";
 	cout << std::endl;
 
-	double arr2[] = { 1.5, 9.5, 7.5, 4.5, 11.5, 100.0, 7.0 };
+	double arr2[] = { 1.5, 9.5, 7.5, 4.5, 11.5, 100.0, 7.0 };//для double
 	int size_arr2 = sizeof(arr2) / sizeof(arr2[0]);
 	cout << "size_arr = " << size_arr2 << std::endl;
 
